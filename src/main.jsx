49dd354cc70ec76410e0021ea7234f457c0841bc
adrianus898989 <adrianus898989@gmail.com>
2026-08-14 19:13:21 +0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,5 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><BrowserRouter basename="/home-staff-management"><App/></BrowserRouter></React.StrictMode>
+  <React.StrictMode><BrowserRouter basename="/wfh-management"><App/></BrowserRouter></React.StrictMode>
 )
